2c4460a8bd1b9ab468943e02c0a7092bb4adab43
lukluk santoso <lukluk.santoso@bankmas.co.id>
2025-07-05 22:26:50 +0700

    update version

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,9 @@ set -e
 COMMAND="$1"
 CONFIG_FILE="$2"
 ADDITIONAL_ARGS="$3"
+DOCKER_USERNAME="$4"
+DOCKER_PASSWORD="$5"
+DOCKER_REGISTRY="$6"
 
 echo "🚀 DeployAja CLI Action"
 echo "Command: $COMMAND"
@@ -21,6 +24,18 @@ fi
 
 if [ -n "$ADDITIONAL_ARGS" ] && [ "$ADDITIONAL_ARGS" != "null" ]; then
     CMD_ARGS="$CMD_ARGS $ADDITIONAL_ARGS"
+fi
+
+if [ -n "$DOCKER_USERNAME" ] && [ "$DOCKER_USERNAME" != "null" ]; then
+    CMD_ARGS="$CMD_ARGS -u $DOCKER_USERNAME"
+fi
+
+if [ -n "$DOCKER_PASSWORD" ] && [ "$DOCKER_PASSWORD" != "null" ]; then
+    CMD_ARGS="$CMD_ARGS -p $DOCKER_PASSWORD"
+fi
+
+if [ -n "$DOCKER_REGISTRY" ] && [ "$DOCKER_REGISTRY" != "null" ]; then
+    CMD_ARGS="$CMD_ARGS -r $DOCKER_REGISTRY"
 fi
 
 # Execute the command
